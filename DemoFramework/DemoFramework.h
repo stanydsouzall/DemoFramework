bbd46124ac18a7cfc8d7289bf1269d38791f0b36
Stany Dsouza <stany.dsouza@mswipe.in>
2021-04-13 10:34:47 +0530

    first commit

---
+++ DemoFramework/DemoFramework.h
@@ -0,0 +1,18 @@
+//
+//  DemoFramework.h
+//  DemoFramework
+//
+//  Created by Stany on 12/04/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DemoFramework.
+FOUNDATION_EXPORT double DemoFrameworkVersionNumber;
+
+//! Project version string for DemoFramework.
+FOUNDATION_EXPORT const unsigned char DemoFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DemoFramework/PublicHeader.h>
+
+
---
+++ DemoFramework/DemoViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DemoViewController.swift
+//  DemoFramework
+//
+//  Created by Stany on 12/04/21.
+//
+
+import UIKit
+
+open class DemoViewController: UIViewController {
+
+    open override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
